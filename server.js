7205943f6754edec15191676eca446c3c2878b1a
Effitech <effitechsolutions.gchu@gmail.com>
2026-05-01 14:28:35 -0300

    Remove debug endpoint

--- server.js
+++ server.js
@@ -218,10 +218,6 @@ Respondé la pregunta basándote en estos resultados. Citá las fuentes relevant
   }
 });
 
-app.get('/api/admin/debug', (req, res) => {
-  res.json({ token_set: !!process.env.ADMIN_TOKEN, token_length: (process.env.ADMIN_TOKEN || '').length });
-});
-
 app.post('/api/admin/reset-ip', (req, res) => {
   const token = req.headers['x-admin-token'];
   if (!token || token !== process.env.ADMIN_TOKEN) {
